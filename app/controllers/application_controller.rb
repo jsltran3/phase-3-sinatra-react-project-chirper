2f0fdaeb081c82d2632e49197b339fb7fae4a53c
Joseph <jsltran@gmail.com>
2022-11-05 00:06:10 -0700

    partially setup app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,24 @@
-class ApplicationController < Sinatra::Base
+class ApplicationController <Sinatra::Base 
   set :default_content_type, 'application/json'
-  
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+
+  post '/chirps' do
+    chirp = Chirp.create(chirp_message: params[:chirp_message], 
+    created_at: params[:created_at], 
+    updated_at: params[:updated_at], 
+    )
   end
 
-end
+  get '/chirper_profiles' do
+    chirper_profiles = Chirper_profile.all
+    chirper_profiles.to_json
+  end 
+
+  get "/chirps" do 
+    chirps = Chirp.all
+    chirps.to_json
+  end
+  
+  # delete '/' do 
+  # end 
+
+end 
